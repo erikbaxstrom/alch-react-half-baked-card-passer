8035d75c380a9cca630863d0589a8d2a93ff8731
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-25 17:24:11 -0800

    skeleton context files set up

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../context/GameContext';
+// import { useGameContext } from '../context/GameContext';
 import CardList from './CardList';
 
 export default function Player({
@@ -10,12 +12,18 @@ export default function Player({
   selectedCard,
   to,
 }) {
+  const { something } = useContext(GameContext);
+  // const { something, setSomething } = useGameContext();
+  // setSomething('set something');
+  console.log(something);
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
-      <p>Player {player}</p>
+      <p>
+        Player {player} {something}
+      </p>
       <CardList
         player={player}
         cards={hand}
---
+++ src/context/GameContext.js
@@ -0,0 +1,22 @@
+import { useState, createContext } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [something, setSomething] = useState('someStartingState');
+  return (
+    <GameContext.Provider value={{ something, setSomething }}>{children}</GameContext.Provider>
+  );
+};
+
+// const useGameContext = () => {
+//   const context = useContext(GameContext);
+
+//   if (context === undefined) {
+//     throw new Error('useGameContext must be within a GameProvider');
+//   }
+
+//   return context;
+// };
+
+export { GameContext, GameProvider };
